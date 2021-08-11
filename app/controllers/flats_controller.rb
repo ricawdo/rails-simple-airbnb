da031b7446b1bc9766794d019fe699494881b66f
Ricardo Dupot <ricawdo@gmail.com>
2021-08-11 18:00:47 +0200

    controller created

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,23 @@
+class FlatsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def add
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
